b0bcf0bed3d7b017eef3f4f9649d4f3e0f64d031
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2023-05-25 09:07:11 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const app = express();
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("<h1>Welcome to Calculation</h1>");
+});
+
+// calculator
+app.get("/calculator", (req, res) => {
+  res.sendFile(__dirname + "/calculator.html");
+});
+app.post("/calculator", (req, res) => {
+  const sum = Number(req.body.num1) + Number(req.body.num2);
+  console.log(sum);
+  res.send(`<h2>Result is = ${sum} </h2>`);
+});
+
+// BMI Calculator
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+  const height = req.body.height;
+  const weight = req.body.weight;
+  const BMI = weight / (height * height);
+
+  res.send(`<h2>BMI is ${BMI}</h2>`);
 });
 
 // server running
